1f915fdced164bdec993f0df56b80a53c72f70ca
MinSooJoh <msjoh84@gmail.com>
2021-08-23 15:49:50 +0900

    fix: created fizzbuzz for python
    - changed independent variable from i to j

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
-for i in range(1,20+1):
-    if i%3==0 or i%5==0:
-        print('fizz'*(i%3==0) + 'buzz'*(i%5==0))
+# Fizz Buzz for Python
+
+for j in range(1,20+1):
+    if j%3==0 or j%5==0:
+        print('fizz'*(j%3==0) + 'buzz'*(j%5==0))
     else:
-        print(i)
+        print(j)
 
